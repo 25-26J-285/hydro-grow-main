58fcdae92f5701d6000991199317d28c18ed0735
imalka2000 <imalkathushan2000@gmail.com>
2026-01-11 23:41:14 +0530

    adding few changes changes

--- frontend/styles/PlantDetails.styles.ts
+++ frontend/styles/PlantDetails.styles.ts
@@ -7,7 +7,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 
-  /* Top Bar */
+  /* Top Bar part*/
   topBar: {
     flexDirection: 'row',
     justifyContent: 'space-between',
